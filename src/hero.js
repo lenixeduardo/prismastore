c3f06ad7bff08349355deabc7689003ff34c7c62
Lenix Eduardo R. Camargo <85527457+lenixeduardo@users.noreply.github.com>
2026-09-14 17:16:22 -0300

    feat: resume dashboard after internal product save

--- src/hero.js
+++ src/hero.js
@@ -35,4 +35,9 @@ function enterDashboard() {
 }
 
 enterButton?.addEventListener('click', enterDashboard);
-loadHeroArtwork();
+
+if (sessionStorage.getItem('prismastore:resume-panel')) {
+  enterDashboard();
+} else {
+  loadHeroArtwork();
+}
